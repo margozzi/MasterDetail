2ec214ef88b1b33c9daca7caf45ad7a389b19bfb
Mike Margozzi <mmargozzi@arubanetworks.com>
2020-02-18 16:08:06 -0800

    Update to latest packages

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 
     return (
       <div>
-        <BrowserRouter>
+        <BrowserRouter basename="/">
           <Suspense fallback={<WaitSpinner />}>
             <Switch>
               <Route exact path="/" component={Home} />
